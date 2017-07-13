761167793c755d56d8485e821b1a67bde5514376
kash <kashiwa.electory@gmail.com>
2017-07-14 00:34:01 +0900

    Add: load.lua Update: mod.txt

---
+++ load.lua
@@ -0,0 +1,25 @@
+if not Steam then
+    return
+end
+
+function executeScript(script)
+    local requiredScript = script:lower()
+    local hook_files = {
+        ["lib/units/interactions/interactionext"] = "InteractTime.lua"
+    }
+    if hook_files[requiredScript] then
+        local fileName = ModPath .. "lua/" .. hook_files[requiredScript]
+        if io.file_is_readable(fileName) then
+            log("[Info] Interact time hint was successfully loaded!")
+            dofile(fileName)
+		else
+			log("[Error] BLT could not open script '" .. fileName .. "'.")
+		end
+    else
+        log("[Error] Unregistered script called: " .. requiredScript)
+    end
+end
+
+if RequiredScript then
+    executeScript(RequiredScript)
+end
