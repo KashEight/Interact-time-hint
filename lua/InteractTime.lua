edbad568fc52d61794722c145140ede834b09c90
kash <kashiwa.electory@gmail.com>
2017-07-27 17:41:29 +0900

    Show timer more smartly

--- lua/InteractTime.lua
+++ lua/InteractTime.lua
@@ -6,6 +6,19 @@ function BaseInteractionExt:selected(player)
 	if self:_timer_value() then
 		interact_timer = self:_get_timer()
 	end
+	if interact_timer < 10 then
+		if string.len(interact_timer) > 3 then
+			interact_timer = string.sub(math.floor(interact_timer * 10 + 0.5) / 10, 1, 3)
+		elseif string.len(interact_timer) == 1 then
+			interact_timer = interact_timer .. ".0"
+		end
+	else
+		if string.len(interact_timer) > 4 then
+			interact_timer = string.sub(math.floor(interact_timer * 10 + 0.5) / 10, 1, 4)
+		elseif string.len(interact_timer) == 2 then
+			interact_timer = interact_timer .. ".0"
+		end
+	end
 	local _string_macros = {}
 	self:_add_string_macros(_string_macros)
 	local basic_text = managers.localization:text(self._tweak_data.text_id, _string_macros)
