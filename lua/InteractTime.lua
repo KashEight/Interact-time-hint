1f721c6f573bbdaab7d4a17240f64a10c0510b3d
kash <kashiwa.electory@gmail.com>
2017-08-12 15:42:01 +0900

    Remove: selected_original var

--- lua/InteractTime.lua
+++ lua/InteractTime.lua
@@ -1,7 +1,4 @@
-local selected_original = BaseInteractionExt.selected
-
 function BaseInteractionExt:selected(player)
-	selected_original(self, player)
 	local interact_timer = 0
 	if self:_timer_value() then
 		interact_timer = self:_get_timer()
