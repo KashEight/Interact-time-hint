ebd965d4a1221528880514aa55faa01560628d0e
kash <kashiwa.electory@gmail.com>
2017-12-16 19:36:47 +0900

    Fix: equipments

--- lua/InteractTime.lua
+++ lua/InteractTime.lua
@@ -1,19 +1,24 @@
 local selected_original = BaseInteractionExt.selected
 
-function BaseInteractionExt:selected(player)
-	selected_original(self, player)
+function BaseInteractionExt:selected(...)
+	selected_original(self, ...)
 
 	local _text_id = self._tweak_data.text_id or alive(self._unit) and self._unit:base().interaction_text_id and self._unit:base():interaction_text_id()
 	local _string_macros = {}
 
 	self:_add_string_macros(_string_macros)
 
-	if _text_id and managers.player:has_special_equipment(self._tweak_data.special_equipment) then
+	if self._tweak_data.special_equipment and not managers.player:has_special_equipment(self._tweak_data.special_equipment) then
+		return true
+	end
+
+	if _text_id then
 		local basic_text = managers.localization:text(_text_id, _string_macros)
 		managers.hud:show_interact({
 			text = basic_text .. " (" .. self:check_interact_time() .. " s)",
 			icon = self._tweak_data.icon
 		})
+	end
 	
 	return true
 end
